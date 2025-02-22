191e95e99b9e0786472516968aa94eb8d0c8d40c
alinkenauger <124498183+alinkenauger@users.noreply.github.com>
2025-02-22 14:06:58 -0500

    Add public-samples/golf-swing-recording--n6oxdk/infrastructure/terraform/modules/vpc/outputs.tf

---
+++ infrastructure/terraform/modules/vpc/outputs.tf
@@ -0,0 +1,27 @@
+# Output definitions for VPC module exposing essential networking information
+# Required by other infrastructure components of the Video Coaching Platform
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the created VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  description = "List of database subnet IDs"
+  value       = aws_subnet.database[*].id
+}
